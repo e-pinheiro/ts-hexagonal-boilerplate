53c763c580b6ea73e537ea927c51057a88076cd5
Erasmo De Souza Pinheiro <erasmodesouzapinheiro@Erasmos-MacBook-Air.local>
2025-03-05 00:39:45 -0300

    refactoring in validation

--- src/adapters/controllers/group/GroupInputValidator.ts
+++ src/adapters/controllers/group/GroupInputValidator.ts
@@ -1,20 +1,7 @@
-import { ValidationRule } from '@/adapters/framework/validators/ValidationRule.interface';
 import { GroupInputValidator } from '../../framework/GroupInputValidator';
-import RequiredFieldValidation from '@/adapters/framework/validators/requiredFieldValidation';
-import StringTypeValidation from '@/adapters/framework/validators/stringTypeValidation';
-import MinLengthValidation from '@/adapters/framework/validators/minLengthValidation';
+import { StringValidator } from '@esp-labs/validators';
 
 export class GroupValidatorImpl implements GroupInputValidator {
-  private readonly validationRules: ValidationRule[];
-
-  constructor() {
-    this.validationRules = [
-      new RequiredFieldValidation('name'),
-      new StringTypeValidation('name'),
-      new MinLengthValidation('name', 3),
-    ];
-  }
-
   validate(input: unknown): void {
     if (!input || typeof input !== 'object') {
       throw new Error('Invalid input');
@@ -22,7 +9,11 @@ export class GroupValidatorImpl implements GroupInputValidator {
 
     const { name } = input as { name?: unknown };
 
-    // Execute todas as validações
-    this.validationRules.forEach(rule => rule.validate(name));
+    // library
+    new StringValidator({
+      fieldName: 'name',
+      minLength: 4,
+      maxLength: 10,
+    }).validate(name);
   }
 }
